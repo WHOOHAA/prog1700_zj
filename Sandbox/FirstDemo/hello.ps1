7724db7dea2c08b8cfe6d186a51b79bcb182df55
W0202409 <W0202409@nscc.ca>
2019-09-08 16:17:09 -0700

    First hello world application

---
+++ Sandbox/FirstDemo/hello.ps1
@@ -0,0 +1,12 @@
+# DO NOT EDIT: The main function to house our program code 
+function main {
+
+    $name = "Mike"
+
+    Write-Output "Hello $name!!"
+    
+	
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
