8471cce1f3507f1b02edfe61ba8cb33d6b159dd1
W0202409 <W0202409@nscc.ca>
2019-09-10 07:39:24 -0700

    Proper Name and Repo

---
+++ InClass/Sept10/first.ps1
@@ -0,0 +1,22 @@
+<#
+Author: Zack Johnson
+Date: 10 Sept 2019
+Description: This program is the first in-class excersice
+#>
+# DO NOT EDIT: The main function to house our program code 
+function main {
+
+    #Input and Variables
+        $numberOne=3
+        $numberTwo=5
+
+    #Processsing
+        $total = $numberOne + $numberTwo
+
+    #output
+        Write-Output "The total is $total"
+	
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Zack Johnson's Repository
-https://github.com/PROG1700-SMnS-ITC2019/prog1700_zj.git
+Private Student Repository for PROG1700
