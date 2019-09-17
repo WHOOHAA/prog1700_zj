36576910a6f175cb8a745a96700592fc24eda295
W0202409 <W0202409@nscc.ca>
2019-09-17 19:30:50 -0300

    Assignment 1 Setup

---
+++ Assignments/Assignment_1/Program2.ps1
@@ -0,0 +1,20 @@
+#Author: Zachary Johnson
+#Date: Sep. 12, 2019
+#Description: Total stock worth
+
+# DO NOT EDIT: The main function to house our program code 
+function main {
+
+    # 1. Input amd variables
+    
+    
+    # 2. Processing
+    
+    
+    # 3. Output
+    
+	
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
