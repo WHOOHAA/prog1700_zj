03897b072eb41564e4d719be97a21df3a1f3fcb4
W0202409 <W0202409@nscc.ca>
2019-09-17 11:22:28 -0300

    Tech Check 1 Completed

---
+++ TechChecks/tc1.ps1
@@ -0,0 +1,27 @@
+#Author: Zachary Johnson
+#Date: Sep. 17, 2019
+#Description: Tech Check 1
+
+# DO NOT EDIT: The main function to house our program code 
+function main {
+
+    # 1. Input amd variables
+    $bill = [float](Read-Host -Prompt "Please enter your original bill amount")
+    Set-Variable TAX -Option Constant -Value 0.15
+    Set-Variable TIP -Option Constant -Value 0.20
+
+    #$tax = 0.15 # hard-coded variable
+    #$tip = 0.20 # hard-coded variable
+
+    # 2. Processing
+    $totalTax = $bill * $TAX
+    $totalTip = $bill * $TIP
+    $totalBill = $bill + $totalTax + $totalTip
+
+    # 3. Output
+    Write-Output ("Your original bill amount {0:C} `n Your tax is: {1:C} `n Your tip is: {2:C} `n Your total is: {3:C}" -f $bill, $totalTax, $totalTip, $totalBill)
+	
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
