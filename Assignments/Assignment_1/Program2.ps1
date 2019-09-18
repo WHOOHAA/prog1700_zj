9bdf6d83158a8b8535d4191c86063fcfbfccaab2
W0202409 <W0202409@nscc.ca>
2019-09-17 21:39:10 -0300

    Completed Project 2 Pseudocode, editing Project 2

--- Assignments/Assignment_1/Program2.ps1
+++ Assignments/Assignment_1/Program2.ps1
@@ -1,19 +1,23 @@
 #Author: Zachary Johnson
-#Date: Sep. 12, 2019
-#Description: Total stock worth
+#Date: Sep. 17, 2019
+#Description: Assignment 1 Project 1 Hipster's Local Vinyl Records
 
 # DO NOT EDIT: The main function to house our program code 
 function main {
 
     # 1. Input, variables and program title output
-    
-    
+    Write-Output "Weekly Loan Calculator `n"
+    $loanAmount = [float](Read-Host -Prompt "Enter the amount of loan")
+    $interestRate = [float](Read-Host -Prompt "Enter the interest rate (%)")
+    $numberYears = [float](Read-Host -Prompt "Enter the number of years")
+
     # 2. Processing
-    
+    $i = $interestRate / 5200
+    $weeklyPayments = ($i / (1 - [math]::pow((1 + $i),-52))) * $numberYears
     
     # 3. Output
-    
-	
+    Write-Output ("`nYour weekly payment will be: {0:C}"-f $weeklyPayments)
+
 }
 
 # DO NOT EDIT: Trigger our main function to launch the program
