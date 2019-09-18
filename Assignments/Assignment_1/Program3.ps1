8d5f53afe0b8a3102ed64665d06c9e542c1ecc56
W0202409 <W0202409@nscc.ca>
2019-09-18 12:50:12 -0300

    Working on Asignment1 Pseudocode3 Program3

--- Assignments/Assignment_1/Program3.ps1
+++ Assignments/Assignment_1/Program3.ps1
@@ -5,14 +5,21 @@
 # DO NOT EDIT: The main function to house our program code 
 function main {
 
-    # 1. Input, variables and program title output
-    
-    
-    # 2. Processing
-    
-    
-    # 3. Output
-    
+   # 1. Input, variables and program title output
+   Write-Output "Imperial To Metric Conversion `n"
+   $tons = [float](Read-Host -Prompt "Enter the amount of tons")
+   $stone = [float](Read-Host -Prompt "Enter the stone")
+   $pounds = [float](Read-Host -Prompt "Enter the pounds")
+   $ounces = [float](Read-Host -Prompt "Enter the ounces")
+
+   # 2. Processing
+   $totalOunces = (35840 * $tons) + (224 * $stone) + (16 * $pounds) + $ounces
+   $totalKilos = $totalOunces / 35.274
+   $totalGrams = $totalOunces / 28.35
+   $metricTons = [math]::Floor($totalKilos / 1000)
+   
+   # 3. Output
+   Write-Output ("`nThe metric weight is {0:N0} metric tons, {1:N2} kilos, and {2:N2} grams" -f $metricTons, $totalKilos, $totalGrams)
 	
 }
 
