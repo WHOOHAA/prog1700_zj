907f525f9037fcfe231f81f7f3d353e00aaf6736
W0202409 <W0202409@nscc.ca>
2019-09-24 11:16:36 -0300

    Tech Check 2 Complete

--- TechChecks/tc2.ps1
+++ TechChecks/tc2.ps1
@@ -7,15 +7,29 @@
 # DO NOT EDIT: The main function to house our program code 
 function main {
 
-    # 1. Input and variables
-    
+    # 1. Input, variables & output
+    Write-Output "Tax Withholding Calculator`n"
+    $weeklySalary = [float](Read-Host -Prompt "Please enter the full amount of your weekly salary")
+    $dependents = [float](Read-Host -Prompt "How many dependents do you have?")
+    Set-Variable PROVINCIALTAX -Option Constant -Value 0.06
+    Set-Variable FEDERALTAX -Option Constant -Value 0.25
+    Set-Variable DEPENDENTDEDUCTION -Option Constant -Value 0.02
+
     
     # 2. Processing
-    
+    $provincialTaxHeld = $weeklySalary * $PROVINCIALTAX
+    $federalTaxHeld = $weeklySalary * $FEDERALTAX
+    $totalDependentDeduction = $weeklySalary * ($dependents * $DEPENDENTDEDUCTION)
+    $totalHeld = $provincialTaxHeld + $federalTaxHeld - $totalDependentDeduction
+    $totalTakeHome = $weeklySalary - $totalHeld
     
     # 3. Output
-    
-	
+    Write-Output ("`nProvincial Tax Withheld: {0:C}" -f $provincialTaxHeld)
+    Write-Output ("Federal Tax Withheld: {0:C}" -f $federalTaxHeld)
+    Write-Output ("Dependent Deduction for $dependents dependents: {0:C}" -f $totalDependentDeduction) 
+    Write-Output ("Provincial Withheld: {0:C}" -f $totalHeld) 
+    Write-Output ("Total Take-Home Pay: {0:C}" -f $totalTakeHome) 
+
 }
 
 # DO NOT EDIT: Trigger our main function to launch the program
