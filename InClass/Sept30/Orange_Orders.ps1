dbf055407e0b26556b695b0b1deb02084ea9fc74
W0202409 <W0202409@nscc.ca>
2019-09-30 10:24:52 -0300

    Orange_Orders,ps1 Working on

--- InClass/Sept30/Orange_Orders.ps1
+++ InClass/Sept30/Orange_Orders.ps1
@@ -1,21 +1,22 @@
 ##################################
 #    Author: Zachary Johnson     #
 #    Date: Sep. 30, 2019         #
-#    Description: Orange Orders   #
+#    Description: Orange Orders  #
 ##################################
 
 <#
 Pseudocode
-1.
-2.
-3.
-4.
-5.
-6.
-7.
-8.
-9.
-10.
+1. Start Program
+2. Output "Orange Order System" followed by a dropped a line, to the user.
+3. Prompt for the number of ounds of oranges wanted to order and store it in a float variable called orangeOrder.
+4. Creat Constant PRICEPERPOUND 1.99
+5. Creat Constant SHIPPINGCOST set to 7.50
+6. Creat Constant SHIPPINGDISCOUNT set to 1.5
+7. Initilize Variable totalShippingCost = SHIPPINGCOST
+8. if >= 100 Calculate totalShippingCost = SHIPPINGCOST - SHIPPINGDISCOUNT
+9. After if ended totalOrder = (orangeOrder * PRICEPERPOUND) + totalShippingCost
+10.Output "drop line, Your price for the oranges including shipping is: " to the user.
+    Program end.
 #> 
 
 
