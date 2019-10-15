522e9787b4349b7eef6d7b344e63da84de9293d5
W0202409 <W0202409@nscc.ca>
2019-10-15 12:30:46 -0300

    Exercise 10 Complete

---
+++ InClass/Oct15/Hogwarts.ps1
@@ -0,0 +1,80 @@
+########################################################
+#    Author: Zachary Johnson                           #
+#    Date: 15 Oct 2019                                 #
+#    Description: Hogwarts Sorting Hat with Functioin  #
+########################################################
+
+function Assign-House($InLastName)
+{
+    
+    $assignedNumber = Get-Random -minimum 1 -maximum 5 # generate a random number between 1 and 4
+    $assignMuggle = Get-Random -minimum 1 -maximum 3 # generate a random number between 1 and 2
+
+        if($InLastName -eq "Potter")
+        {
+            $assignedNumber = 1
+            $assignMuggle = 1
+        }
+        elseif($InLastName -eq "Malfoy")
+        {
+            $assignedNumber = 4
+            $assignMuggle = 1
+        }
+        
+            
+            if($assignMuggle -eq 1)
+            {
+                if($assignedNumber -eq 1)
+                {
+                    $assignedHouse = "Gryffindor"
+                }
+                elseif ($assignedNumber -eq 2) 
+                {
+                    $assignedHouse = "Hufflepuff"    
+                }
+                elseif ($assignedNumber -eq 3) 
+                {
+                    $assignedHouse = "Ravenclaw"    
+                }
+                elseif ($assignedNumber -eq 4) 
+                {
+                    $assignedHouse = "Slytherin"        
+                }
+                
+            }
+            elseif ($assignMuggle -eq 2)
+            {
+                $assignedHouse = "Muggle"
+            }
+        
+    return $assignedHouse
+}
+
+
+
+# DO NOT EDIT: The main function to house our program code 
+function main {
+
+    # 1. Input and variables
+    $lastName = Read-Host -Prompt "Enter your last name"
+
+    
+    # 2. Processing
+    $CurrentAssignedHouse = Assign-House -InLastName $lastname
+           
+        if($CurrentAssignedHouse -ne "muggle")
+        {
+            Write-Output "The sorting Hat has assigned you to $CurrentAssignedHouse!"
+        }
+        elseif($CurrentAssignedHouse -eq "Muggle")
+        {
+            Write-Output "You are a Muggle. You will be put on the train back to Platform 9 3/4 (How did they get on the train in the first place if you are Muggle?)"
+        }          
+    
+    # 3. Output
+        Write-Output "Thanks for using the Hogwarts Sorting Hat!"
+	
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
