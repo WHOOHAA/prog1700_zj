700e52627bf3f523223b8bc71a1a65f432c357d5
W0202409 <W0202409@nscc.ca>
2019-10-19 21:28:10 -0300

    LandscapeCalculator.ps1 Complete

--- Assignments/Assignment_2/LandscapeCalculator.ps1
+++ Assignments/Assignment_2/LandscapeCalculator.ps1
@@ -4,14 +4,15 @@
 #    Description: Landscape Calculator Assignment 2 - Project 1   #
 ###################################################################
 
-
-
-
 # DO NOT EDIT: The main function to house our program code 
 function main {
 
     # Input/Output and Variables
-    
+    $houseNumber = [int](Read-Host -Prompt "Enter House Number")
+    $propertyDepth = [int](Read-Host -Prompt "Enter property depth (feet)")
+    $propertyWidth = [int](Read-Host -Prompt "Enter property width (feet)")
+    $grassType = Read-Host -Prompt "Enter type of grass (fescue, bentgrass, campus)"
+    $treeNumber = [int](Read-Host -Prompt "Enter the number of trees")
 
     # Initialize Variables
     
@@ -20,10 +21,43 @@ function main {
 
     
     # Processing
+    $propertySize = $propertyDepth * $propertyWidth
+
+    if($propertySize -gt 5000)
+    {
+        $overSizeCharge = 500
+    }
+    else
+    {
+        $overSizeCharge = 0
+    }
+
+    if($grassType -eq "fescue")
+    {
+        $grassCost = 0.05
+    }
+    elseif ($grassType -eq "bentgrass") 
+    {
+        $grassCost = 0.02
+    }
+    elseif($grassType -eq "campus")
+    {
+        $grassCost = 0.01
+    }
+    else 
+    {
+    $totalCost = "ERROR"
+    }
     
-    
-    # Output
-    
+
+
+
+    if($totalCost -ne "ERROR")
+    {
+        $totalCost = ($propertySize * $grassCost) + ($treeNumber * 100) + 1000 + $overSizeCharge
+    }
+        # Output
+    Write-Output ("Total cost for house {0} is: {1:C}" -f $houseNumber, $totalCost)
 	
 }
 
