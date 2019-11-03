37a95c345ef35a7f03ca4ffa5046358cb95612c4
W0202409 <W0202409@nscc.ca>
2019-11-03 19:54:15 -0400

    Assignmnet 3 Program 1 updated

--- Assignments/Assignment_3/TimeSheet.ps1
+++ Assignments/Assignment_3/TimeSheet.ps1
@@ -4,31 +4,43 @@
 #    Description: Time Sheet Assignment 3 - Project 1   #
 #########################################################
 
-function Check-Input ($INPUTIN)
+function Check-Input ($INUSERINPUT)
 {
     $numberRegEx = "^\d+$"
-
+    if($INUSERINPUT -Match $numberRegEx)
+    {
+        $OUTERROR = $false
+    }
+    else 
+    {
+        $OUTERROR = $true
+    }
+    return $OUTERROR
 }
 
 # DO NOT EDIT: The main function to house our program code 
 function main {
     $hoursWorked = @()
+    
     for ($i = 1; $i -lt 6; $i++)
     {
+        $error = $false
         do
         {
-            Read-Host "Enter hours worked on Day $i"
-            
-            $hoursWorked =+ $userInput
-
+            $userInput = Read-Host "Enter hours worked on Day #$i"
+            $error = Check-Input $INUSERINPUT $userInput
             if($error -eq $true)
             {
-                Write-Output "ERROR try again"
+                Write-Output "ERROR"
+            }
+            else
+            {
+                $hoursWorked += $userInput
             }
         }
         While($error -ne $false)
     }
-	
+	Write-Output "$hoursWorked"
 }
 
 # DO NOT EDIT: Trigger our main function to launch the program
