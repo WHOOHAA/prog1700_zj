afea3011425fb16746ab6753d249a7e92ca42ed2
W0202409 <W0202409@nscc.ca>
2019-11-03 15:30:42 -0400

    Assignment 3 Files Created

---
+++ Assignments/Assignment_3/Program1.ps1
@@ -0,0 +1,31 @@
+###################################################################
+#    Author: Zachary Johnson                                      #
+#    Date: Nov. 03, 2019                                          #
+#    Description: Landscape Calculator Assignment 3 - Project 1   #
+###################################################################
+
+
+
+
+# DO NOT EDIT: The main function to house our program code 
+function main {
+
+    # Input/Output and Variables
+    
+
+    # Initialize Variables
+    
+    
+    # Constant
+
+    
+    # Processing
+    
+    
+    # Output
+    
+	
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
