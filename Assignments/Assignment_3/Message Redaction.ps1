dfdb976926de0b9ff8bece303387a56f020a16e0
W0202409 <W0202409@nscc.ca>
2019-11-05 12:27:13 -0400

    MessageRedaction Updated

--- Assignments/Assignment_3/Message Redaction.ps1
+++ Assignments/Assignment_3/Message Redaction.ps1
@@ -10,7 +10,70 @@
 # DO NOT EDIT: The main function to house our program code 
 function main {
 
-    # $testString = "The rain in spain falls mainly on the plain"
+    
+
+    # $redactArray = @()
+    # $redactedArray = @()
+    $userPhrase = "Imagination is more important than knolege."
+    $userRedact = "a,i,k,j"
+    
+    $phraseArray = $userPhrase.ToCharArray()
+    $redactArray = $userRedact -split ","
+
+    # for($i = 0; $i -lt $redactArray.Length; $i++)
+    # {
+    #    $redactedPhrase = $userPhrase -replace $redactArray[$i], "_"
+    # }
+
+    # compare-object -referenceobject $userPhrase -differenceobject $redactedPhrase
+    
+    $redactedPhrase = ""
+    
+    
+    for($i = 0; $i -lt $phraseArray.Length; $i++)
+    {
+        # CHECK IF CURRENT LETTER IN LIST TO REMOVE
+        if($redactArray -Contains $phraseArray[$i])
+        {
+            $redactedPhrase += "_"
+            $count++
+        }
+        else 
+        {
+            $redactedPhrase += $phraseArray[$i]    
+        }
+
+
+        # for($j = 0; $j -lt $redactArray.Length; $j++)
+        # {
+        #     if($phraseArray[$i] -eq  $redactArray[$j])
+        #     {
+        #         $redactedArray += $phraseArray[$i] -replace $redactArray[$j]
+        #     }
+        #     else
+        #     {
+        #         $redactedArray += $phraseArray[$i]
+        #     }
+        # }
+    }
+    
+    Write-Output $redactedPhrase
+	
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
+
+
+
+
+
+
+
+
+
+
+# $testString = "The rain in spain falls mainly on the plain"
 
     # #Write-Output ($testString -replace "a","-")
 
@@ -25,12 +88,3 @@ function main {
     #     Write-Output $testString[$i]
     #     }
     # }
-
-    $testString2 = "a,b,c,d"
-    $letterArray = $testString2 -split ","
-    Write-Output = $letterArray
-	
-}
-
-# DO NOT EDIT: Trigger our main function to launch the program
-main
