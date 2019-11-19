bb78b91b30a92359ee31e6da865e65d44dd91421
W0202409 <W0202409@nscc.ca>
2019-11-19 10:12:30 -0400

    Assignment 4 Project 1 Started

---
+++ Assignments/Assignment_4/ATeamFileAlteration.ps1
@@ -0,0 +1,24 @@
+#####################################################################
+#    Author: Zachary Johnson                                        #
+#    Date: Nov, 18, 2019                                            #
+#    Description: A Team File Alteration Assignment 4 - Project 1   #
+#####################################################################
+
+
+
+
+# DO NOT EDIT: The main function to house our program code 
+function main {
+
+$inputFile = "ateam_Original.txt"
+$outpuFile =  
+
+$fileDataToAlter = Get-Content $inputFile
+
+$fileDataToAlter
+
+
+}
+
+# DO NOT EDIT: Trigger our main function to launch the program
+main
