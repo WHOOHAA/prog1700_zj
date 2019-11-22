78c8f524e378a26408cbd14b4b5f216b151761ba
W0202409 <W0202409@nscc.ca>
2019-11-21 22:06:37 -0400

    ASSIGNMENT 4 PROGRAM 2 STARTED

--- Assignments/Assignment_4/TheItsyBitsyArdvark.ps1
+++ Assignments/Assignment_4/TheItsyBitsyArdvark.ps1
@@ -17,7 +17,7 @@ function Catch-Error($InInputTextFile, $InInputCSVFile, $InOutputFile, $OutFileD
         # IF ERROR CATCH THEN OUTPUT TEXT ONLY AND EXIT PROGRAM
         try
         {
-            $OutFileDataToAlter = Get-Content $InInputFile
+            $OutFileDataToAlter = Get-Content $InInputTextFile
         }
         catch [System.IO.DirectoryNotFoundException]
         {
@@ -27,7 +27,7 @@ function Catch-Error($InInputTextFile, $InInputCSVFile, $InOutputFile, $OutFileD
         catch
         {
             Write-Output "Unspecified Error"
-            EXIT
+            # EXIT
         }    
         # IF RUNS THROUGH THEN RETURN THE TXT FILE DATA TO THE MAIN FUNCTION
         RETURN $OutFileDataToAlter
@@ -37,7 +37,7 @@ function Catch-Error($InInputTextFile, $InInputCSVFile, $InOutputFile, $OutFileD
         # IF ERROR CATCH THEN OUTPUT TEXT ONLY AND EXIT PROGRAM
         try
         {
-            $OutFileDataToAlter = Get-Content $InInputFile
+            $OutFileDataToAlter = Import-Csv $InInputCSVFile
         }
         catch [System.IO.DirectoryNotFoundException]
         {
@@ -47,47 +47,12 @@ function Catch-Error($InInputTextFile, $InInputCSVFile, $InOutputFile, $OutFileD
         catch
         {
             Write-Output "Unspecified Error"
-            EXIT
+            # EXIT
         }    
         # IF RUNS THROUGH THEN RETURN THE TXT FILE DATA TO THE MAIN FUNCTION
         RETURN $OutFileDataToAlter
     }
-    # IF PASSED IN VARIABLE IS INOUTPUTFILE THEN CONTINUE
-    # elseif($InOutputFile -match $numberRegEx)
-    # {
-    #     # TRYS TO CLEAR CONTENT FORM ALTERED TEXT FILE
-    #     # IF FILE DOESNT EXISTING CATCH AND OUTPUT NOTHING
-    #     try
-    #     {
-    #         Clear-Content -Path $outputFile -ErrorAction Stop
-    #     } 
-    #     catch
-    #     {
-            
-    #     }
-        
-        # SAVES THE ALTERED TEXT TO THE NEW ALTERD FILE
-        # IF ERROR CATCH AND OUTPUT TEXT ONLY AND EXIT
-#         try
-#         {
-#             $alteredData -join "`n" | Add-Content -Path $outputFile -ErrorAction Stop
-#         }
-#         catch [System.UnauthorizedAccessException]
-#         {
-#             Write-Output "You dont have access to that folder"
-#             EXIT
-#         }    
-#         catch [System.IO.DirectoryNotFoundException]
-#         {
-#             Write-Output "That directory does not exist"
-#             EXIT
-#         }
-#         catch
-#         {
-#             Write-Output "Unspecified Error writing file"
-#             EXIT
-#         }
-#     }
+   
 }
 
 # DO NOT EDIT: The main function to house our program code 
@@ -96,70 +61,29 @@ function main {
     # INITILIZING VARIABLES
     $inputTextFile = "the_choices_file.txt"
     $inputCSVFile = "the_choices_file.csv"
-    #$outputFile =  "ateam_Altered.txt"
-    $lineOmit = 0
-    $alteredData = @()
     
     # SENDS INPUTFILE AS ININPUTFILE 
     # TO CATCH-ERROR FUNCTION
-    $storyArray = Catch-Error -InInputFile $inputTextFile
-    $choicesArray = Catch-Error -InInputFile $inputCSVFile
-    # HEADER STATING THE BELOW TEXT IS THE ORIGIONAL FORM THE TXT FILE
-    # Write-Output ("-" * 40)
-    # Write-Output "Origional Text"
-    # Write-Output ("-" * 40)
+    $storyText = Catch-Error -InInputTextFile $inputTextFile
+    $choicesCSV = Catch-Error -InInputCSVFile $inputCSVFile
     
-    # OUTPUT THE ORIGIONAL TEXT
-    # Write-Output $fileDataToAlter
-    # Write-Output ""
-    
-    # RANDOM NUMBER IS GENERATED BETWEEN 1 AND THE LAST ARRAY LINE NUMBER TO OMIT
-    # $lineOmit = Get-Random -Minimum 1 -Maximum ($fileDataToAlter.Length + 1)
-
-    # HEADER STATING THE BELOW TEXT IS ALTERED
-    # Write-Output ("-" * 40)
-    # Write-Output "Altered Text"
-    # Write-Output ("-" * 40)
     
     # FORLOOP CYCLING THOUGH EACH LINE IN THE ORIGIONAL TEXT ARRAY
-    for($i = 0; $i -lt $fileDataToAlter.Length; $i++)
+    for($i = 0; $i -lt $choicesCSV.Length; $i++)
     {
-        # ADDING 1 TO THE CURRENT CYCLE TO BECOME THE NEW LINE NUMBER
-        $lineNumber = $i + 1
-        # SETS LINE NUMBER WITH FORMATTING AS A STING FOR OUTPUT FOR LATER AS LINENUMBEROUTPUT
-        $lineNumberOutput = [string]($lineNumber) + ": "
 
-        # IF LINENUMER MATCHES LINEOMIT
-        # SET LINENUMBEROUTUT AND FILEDATA THIS CYCLE AS ALTEREDLINE
-        if($lineNumber -match $lineOmit)
+        for($j = 0; $j -lt $choicesCSV[$i,$j].Length; $j++)
         {
-            $lineNumberOutput = ""
-            $fileDataToAlter[$i] = ""
-            $alteredLine = $lineNumberOutput + $fileDataToAlter[$i]
-        }
-        # IF FILEDATATOALTER THIS CYCLES LENGTH IS LESS OR EQUAL TO 20
-        # ADD LINENUMBEROUTPUT AND AND FILEDATATOALTER THIS CYCLE AS UPPER TO ALTEREDLINE
-        elseif ($fileDataToAlter[$i].Length -le 20) 
-        {
-            $alteredLine = $lineNumberOutput + $fileDataToAlter[$i].ToUpper()
-        }
-        # IF FILEDATATOALTER THIS CYCLES LENGTH IS GREATER THEN 20
-        # ADD LINENUMBEROUTPUT AND AND FILEDATATOALTER THIS CYCLE AS LOWER TO ALTEREDLINE
-        elseif ($fileDataToAlter[$i].Length -gt 20) 
-        {
-            $alteredLine = $lineNumberOutput + $fileDataToAlter[$i].ToLower()
+            # ADDING 1 TO THE CURRENT CYCLE TO BECOME THE NEW LINE NUMBER
+            $choiceNumber = $j + 1
+            # $choiceNumberOutput = [string]($choiceNumber) + ") "
+
+            Write-Output ("{0}) {1}" -f $choiceNumber, $choicesCSV.[$i,$j])
+            
         }
 
-        # ADD ALTEREDLINE TO ALTEREDS DATA ARRAY
-        $alteredData += $alteredLine
     }
 
-    # OUTPUT THE ALTEREDDATA ARRAY
-    Write-Output $alteredData
-
-    # SENDS OUTPUTFILE AS INOUTPUTFILE 
-    # AND SENDS ALTEREDDATA AS OUTFILEDATATOALTER TO CATCH-ERROR FUNCTION
-    # Catch-Error -InOutputFile $outputFile -OutFileDataToAlter $alteredData
 }
 
 # DO NOT EDIT: Trigger our main function to launch the program
