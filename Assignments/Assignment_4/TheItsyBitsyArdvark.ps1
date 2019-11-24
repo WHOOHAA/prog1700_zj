6298f51246c34d2a868df59a1fd3098613dd5a15
W0202409 <W0202409@nscc.ca>
2019-11-24 18:37:59 -0400

    WOrking on TheItsyBitsy

--- Assignments/Assignment_4/TheItsyBitsyArdvark.ps1
+++ Assignments/Assignment_4/TheItsyBitsyArdvark.ps1
@@ -37,7 +37,7 @@ function Catch-Error($InInputTextFile, $InInputCSVFile, $InOutputFile, $OutFileD
         # IF ERROR CATCH THEN OUTPUT TEXT ONLY AND EXIT PROGRAM
         try
         {
-            $OutFileDataToAlter = Import-Csv $InInputCSVFile
+            $OutFileDataToAlter = Get-Content  $InInputCSVFile
         }
         catch [System.IO.DirectoryNotFoundException]
         {
@@ -61,28 +61,173 @@ function main {
     # INITILIZING VARIABLES
     $inputTextFile = "the_choices_file.txt"
     $inputCSVFile = "the_choices_file.csv"
+    $dataCSV = @()
+    $tempDataCSV = @()
+    $choicesCSV = @()
+    $choiceAnimal = @()
+    $choiceActionOne = @()
+    $choiceAdjective = @()
+    $choiceActionTwo = @()
+    $choiceNoun = @()
+    $choiceActionThree = @()
+    $choiceAdverb = @()
+
+
     
     # SENDS INPUTFILE AS ININPUTFILE 
     # TO CATCH-ERROR FUNCTION
     $storyText = Catch-Error -InInputTextFile $inputTextFile
-    $choicesCSV = Catch-Error -InInputCSVFile $inputCSVFile
+    $dataCSV = Catch-Error -InInputCSVFile $inputCSVFile
     
-    
-    # FORLOOP CYCLING THOUGH EACH LINE IN THE ORIGIONAL TEXT ARRAY
-    for($i = 0; $i -lt $choicesCSV.Length; $i++)
+    foreach($data in $dataCSV)
     {
-
-        for($j = 0; $j -lt $choicesCSV[$i,$j].Length; $j++)
-        {
-            # ADDING 1 TO THE CURRENT CYCLE TO BECOME THE NEW LINE NUMBER
-            $choiceNumber = $j + 1
-            # $choiceNumberOutput = [string]($choiceNumber) + ") "
-
-            Write-Output ("{0}) {1}" -f $choiceNumber, $choicesCSV.[$i,$j])
-            
-        }
-
+        $choicesCSV += $data -split ","
     }
+
+    for ($i = 0; $i -lt $choicesCSV.length; $i++)
+    {
+        if($i -le 5)
+        {
+            $choiceAnimal += $choicesCSV[$i]
+        }
+        elseif($i -le 11)
+        {
+            $choiceActionOne += $choicesCSV[$i]
+        }
+        elseif($i -le 17)
+        {
+            $choiceAdjective += $choicesCSV[$i]
+        }
+        elseif($i -le 23)
+        {
+            $choiceActionTwo += $choicesCSV[$i]
+        }
+        elseif($i -le 29)
+        {
+            $choiceNoun += $choicesCSV[$i]
+        }
+        elseif($i -le 35)
+        {
+            $choiceActionThree += $choicesCSV[$i]
+        }
+        elseif($i -le 42)
+        {
+            $choiceAdverb += $choicesCSV[$i]
+        }
+    }
+
+    # for ($i = 7; $i -le 13; $i++)
+    # {
+
+    #     $choiceAnimal = @()
+
+
+    # }
+
+
+    # for ($i = 14; $i -le 20; $i++)
+    # {
+
+    #     $choiceActionOne = @()
+
+
+    # }
+
+
+    # for ($i = 0; $i -le 6; $i++)
+    # {
+
+    #     $choiceAdjective = @()
+
+
+    # }
+
+
+    # for ($i = 0; $i -le 6; $i++)
+    # {
+    #     $choiceActionedTwo = @()
+
+
+
+    # }
+
+    
+    # for ($i = 0; $i -le 6; $i++)
+    # {
+
+    #     $choiceNoun = @()
+
+
+    # }
+
+
+    
+    # for ($i = 0; $i -le 6; $i++)
+    # {
+
+    #     $choiceActionThree = @()
+
+
+    # }
+
+
+    
+    # for ($i = 0; $i -le 6; $i++)
+    # {
+
+    #     $choiceAdverb = @()
+
+
+    # }
+
+
+
+    # for($i = 0; $i -lt $dataCSV.Length; $i++)
+    # {
+    #     $tempDataCSV[$i] = $dataCSV[$i] -split ","
+
+
+    #     for($j = 0; $j -lt $dataCSV.Length; $j++)
+    #     {
+    #         $choicesCSV[$i][$j] = $tempDataCSV[$j] 
+    #     }
+    # }
+
+
+    # for($i = 0; $i -lt $dataCSV.Length; $i++)
+    #         {
+    #         $choicesCSV += ""
+    #         }
+
+    # for($i = 0; $i -lt $dataCSV.Length; $i++)
+    #         {
+    #         $choicesCSV$i += $dataCSV[$i] -split ","
+    #        }
+
+    # FORLOOP CYCLING THOUGH EACH LINE IN THE ORIGIONAL TEXT ARRAY
+    # for($i = 0; $i -lt $choicesCSV.Length; $i++)
+    # {
+
+    #     for($j = 0; $j -lt $choicesCSV[$i].Length; $j++)
+    #     {
+    #         # ADDING 1 TO THE CURRENT CYCLE TO BECOME THE NEW LINE NUMBER
+    #         $choiceNumber = $j + 1
+    #         # $choiceNumberOutput = [string]($choiceNumber) + ") "
+
+    #         Write-Output ("{0}) {1}" -f $choiceNumber, $choicesCSV[$i][$j])
+            
+    #     }
+
+    # }
+
+
+
+
+    # for($i = 0; $i -lt $storyText.Length; $i++)
+    # {
+    #     ($choiceOne, $choiceTwo, $choiceThree, $choiceFour, $choiceFive, $choiceSix)
+
+    # }
 
 }
 
