1253673c165c62e7593b88ad6846dee0770e86b4
W0202409 <W0202409@nscc.ca>
2019-11-26 03:13:01 -0400

    Batleship.ps1 ERROR fIX

--- Assignments/Assignment_4/Battleship.ps1
+++ Assignments/Assignment_4/Battleship.ps1
@@ -129,7 +129,7 @@ function main {
                         $error = $true
                     }
                 }
-                While($error -eq $true -or $hits -ne $enemyCount)
+                While($error -eq $true -and $hits -ne $enemyCount)
                 
                 # IF INPUT IS NT BETWEEN 2 AND 3 CHARACTERS IN LENGTH THEN SET ERROR TO TRUE
                 if($attack.length -lt 2 -or $attack.length -gt 3)
@@ -298,7 +298,7 @@ function main {
 
                 
             }
-            While($error -eq $true -or $hits -ne $enemyCount)
+            While($error -eq $true -and $hits -ne $enemyCount)
 
             # IF HIT OUTPUT HIT!!!!!! AND X MARKS THE SPOT ON MAPVIEW
             if($map[$attackMap[1]][$attackMap[0]] -eq 1)
@@ -327,7 +327,7 @@ function main {
         }
         
     }
-    While($misslesRemaining -ne 0  -or $hits -ne $enemyCount)
+    While($misslesRemaining -ne 0 -and $hits -ne $enemyCount)
     
     # FINAL OUTPUT WIN
     if($hits -eq $enemyCount)
